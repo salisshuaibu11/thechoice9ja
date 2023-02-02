f6548ebf90f5b77073f75c5950149b301de2a4ac
Salisu Shuaibu <salisshuaibu11@gmail.com>
2023-02-02 10:40:49 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'thechoice9ja',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
